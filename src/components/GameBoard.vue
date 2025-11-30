f9866da1f8baf67cd000d6f36d54aa0c8594ae0f
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2025-11-30 18:24:48 +0100

    create board grid

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import SudokuBlock from './SudokuBlock.vue'
-
-const { title = 'Sudoko!' } = defineProps<{
-  title?: string
-}>()
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
-  <div class="game-board">
-    <SudokuBlock v-for="block in 9" :key="block" />
+  <div class="board">
+    <SudokuBlock v-for="n in 9" :key="n" />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.board {
+  display: grid;
+  width: calc(100vmin - 4rem);
+  height: calc(100vmin - 4rem);
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+}
+</style>
