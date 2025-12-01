dcbfd564e2db29988bc8ed16b8dbf9740b03f69a
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2025-12-01 10:59:06 +0100

    Add pinia

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+import { useUserStore } from '@/store/useUserStore'
 import SudokuBlock from './SudokuBlock.vue'
+import UserForm from './UserForm.vue'
+const userStore = useUserStore()
 </script>
 
 <template>
+  <h1>{{ userStore.display }}</h1>
+  <UserForm />
   <div class="board">
     <SudokuBlock v-for="n in 9" :key="n" />
   </div>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useUserStore } from '@/store/useUserStore'
+const userStore = useUserStore()
+</script>
+
+<template>
+  <input type="text" v-model="userStore.name" />
+</template>
+
+<style scoped></style>
