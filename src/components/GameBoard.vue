cbd7150250433232dbcd6ee74cbaeb74eab5d907
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2025-12-29 11:42:11 +0100

    add TanStack Vue Query

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -2,15 +2,26 @@
 import { transformArray } from '@/utils/transformArray'
 import SudokuBlock from './SudokuBlock.vue'
 import { useGameStore } from '@/store/useGameStore'
+import { useQuery } from '@tanstack/vue-query'
 const gameStore = useGameStore()
-const url = 'https://sugoku.onrender.com/board?difficulty=easy'
-const res = await fetch(url)
-const data = await res.json()
-gameStore.board = transformArray(data.board)
+const { isLoading } = useQuery({
+  queryKey: ['sudoku-board'],
+  queryFn: async () => {
+    const url = 'https://sugoku.onrender.com/board?difficulty=easy'
+    const res = await fetch(url)
+    const data = await res.json()
+    if (data?.board) {
+      gameStore.board = transformArray(data.board)
+    }
+
+    return data
+  },
+})
 </script>
 
 <template>
-  <div class="board">
+  <div v-if="isLoading">Loading...</div>
+  <div class="board" v-if="gameStore.board.length">
     <SudokuBlock v-for="n in 9" :key="n" :sudoku-block-index="n" />
   </div>
 </template>
